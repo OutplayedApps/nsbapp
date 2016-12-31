8e32f137e885c5c06afac934fbfad4084bfe6d05
Ashwin Ramaswami <aramaswamis@gmail.com>
2016-12-31 17:32:38 -0500

    side menu improvements

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -11,5 +11,14 @@ angular.module('IonicGulpSeed')
     .controller('MainController', function($scope) {
 
         // do something with $scope
+        if (!$scope.mode) $scope.mode = 1;
+        if (!$scope.level) $scope.level = 1;
+        //todo: get from memory.
+        $scope.toggleMode = function(m) {
+            if (m) $scope.mode = m;
+        }
+        $scope.toggleLevel = function(l) {
+            if (l) $scope.level = l;
+        }
 
     });
