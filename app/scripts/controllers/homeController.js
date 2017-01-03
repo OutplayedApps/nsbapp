f42604b76af25e114aec79ca23459df97b4ec3d1
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-01-03 11:24:42 -0500

    facebook banner ads only

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -185,7 +185,7 @@ angular.module('IonicGulpSeed')
                                         bonusQ: $scope.data.bonusQ
                                     };
 
-                                    $scope.fullQuestion = $scope.dataReal.tossupQ.split(" ");
+                                    $scope.fullQuestion = $scope.dataReal.tossupQ.split(/[ ,+,-,×,\/]/); //math split.
                                     $scope.data.tossupQ = "";
                                     var index = 0;
                                     var initialReadSpeed = 1000/SettingsService.settings.readSpeed;
