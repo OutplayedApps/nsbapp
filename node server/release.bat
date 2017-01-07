dc792165a643b510463eac5bbcd7f89a09cd3229
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-01-07 16:06:59 -0500

    1.1.0 stuff

--- node server/release.bat
+++ node server/release.bat
@@ -1,8 +1,11 @@
 cd  C:/Users/arama/Documents/WebStormProjects/nsbapp
 ionic build --release
 cd platforms/android/build/outputs/apk/
-jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.comodoca.com/rfc3161 -digestalg SHA1 -keystore nsbapp.keystore android-release-unsigned.apk NSBApp
-zipalign -v 4 android-release-unsigned.apk release-1.0.1.apk
+jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.comodoca.com/rfc3161 -digestalg SHA1 -keystore nsbapp.keystore android-x86-release-unsigned.apk NSBApp
+jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.comodoca.com/rfc3161 -digestalg SHA1 -keystore nsbapp.keystore android-armv7-release-unsigned.apk NSBApp
+
+zipalign -v 4 android-x86-release-unsigned.apk release-x86-1.1.0.apk
+zipalign -v 4 android-armv7-release-unsigned.apk release-armv7-1.1.0.apk
 
 exit
 
