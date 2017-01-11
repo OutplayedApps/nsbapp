935ef381cbb6d46b0dd69f36594a5d9e8148a05d
Ashwin Ramaswami <aramaswamis@gmail.com>
2017-01-10 20:17:54 -0500

    cordova

--- node server/release.bat
+++ node server/release.bat
@@ -4,8 +4,8 @@ cd platforms/android/build/outputs/apk/
 jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.comodoca.com/rfc3161 -digestalg SHA1 -keystore nsbapp.keystore android-x86-release-unsigned.apk NSBApp
 jarsigner -verbose -sigalg SHA1withRSA -tsa http://timestamp.comodoca.com/rfc3161 -digestalg SHA1 -keystore nsbapp.keystore android-armv7-release-unsigned.apk NSBApp
 
-zipalign -v 4 android-x86-release-unsigned.apk release-x86-1.2.0.apk
-zipalign -v 4 android-armv7-release-unsigned.apk release-armv7-1.2.0.apk
+zipalign -v 4 android-x86-release-unsigned.apk release-x86-1.2.1.apk
+zipalign -v 4 android-armv7-release-unsigned.apk release-armv7-1.2.1.apk
 
 exit
 
@@ -13,3 +13,8 @@ gulp -b &  code-push release-cordova NSBApp android
 code-push promote NSBApp Staging Production
 
 gulp -b &  code-push release-cordova NSBApp android & code-push promote NSBApp Staging Production
+
+
+gulp -b &  code-push release-cordova NSBApp ios 1.2.0 & code-push promote NSBApp Staging Production
+
+code-push release-cordova NSBApp ios -t 1.2.0 -x & code-push promote NSBApp Staging Production
