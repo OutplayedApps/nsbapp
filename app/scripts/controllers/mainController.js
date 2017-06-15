0727ae20cf03bb25f863e14ed59bff2a4a39e880
arvindr9 <saramaswami@gmail.com>
2017-06-15 10:58:15 -0400

    Added numbers to the dropdowns

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -291,5 +291,7 @@ angular.module('IonicGulpSeed')
         $scope.closeModal = function() {
             $scope.modal.hide();
         };
+        $scope.setNums=[1,2,3,4,5,6,7,8];
+        $scope.packetNums=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
 
     });
